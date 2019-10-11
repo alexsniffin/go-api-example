865626f1117c67efa854e9edd144f07a8de92379
alexsniffin <alexsniffin@gmail.com>
2019-10-11 17:58:27 -0400

    format

--- internal/api/handlers/todo_test.go
+++ internal/api/handlers/todo_test.go
@@ -10,12 +10,12 @@ import (
 	"github.com/unrolled/render"
 )
 
-type TestTodoStore struct {}
+type TestTodoStore struct{}
 
 func (t *TestTodoStore) GetTodo(id int) (models.Todo, error) {
 	return models.Todo{
-		ID: 1,
-		Todo: "test",
+		ID:        1,
+		Todo:      "test",
 		CreatedOn: "test",
 	}, nil
 }
@@ -37,8 +37,8 @@ func TestHealthCheckHandler(t *testing.T) {
 	}
 
 	q := req.URL.Query()
-    q.Add("id", "1")
-    req.URL.RawQuery = q.Encode()
+	q.Add("id", "1")
+	req.URL.RawQuery = q.Encode()
 
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(todoHandler.HandleGet)
@@ -46,13 +46,13 @@ func TestHealthCheckHandler(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
-        t.Errorf("handler returned wrong status code: got %v want %v",
-            status, http.StatusOK)
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
 	}
-	
+
 	expected := `{"id":1,"todo":"test","created_on":"test"}`
-    if rr.Body.String() != expected {
-        t.Errorf("handler returned unexpected body: got %v want %v",
-            rr.Body.String(), expected)
-    }
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
 }
